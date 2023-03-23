8c83e87179aa99c9af06061cd9c377b63509cb3e
Surya-2811 <84892906+surya-2811@users.noreply.github.com>
2023-03-23 17:59:04 +0530

    added cart functions in useContext

---
+++ src/pages/Store.tsx
@@ -0,0 +1,22 @@
+import { Row, Col } from "react-bootstrap";
+import StoreItem from "../components/StoreItem";
+import StoreItems from "../data/items.json";
+
+const Store = () => {
+  return (
+    <>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {StoreItems.map((item) => {
+          return (
+            <Col key={item.id}>
+              <StoreItem {...item} />
+            </Col>
+          );
+        })}
+      </Row>
+    </>
+  );
+};
+
+export default Store;
